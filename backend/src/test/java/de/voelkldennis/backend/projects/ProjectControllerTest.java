db089aee4060c6740808f429549693afa6929e57
dennis-aclue <voelkldennis@googlemail.com>
2022-11-21 14:23:15 +0100

    add ProjectUtils Test for UUID for Sonar Cloud

---
+++ backend/src/test/java/de/voelkldennis/backend/projects/ProjectControllerTest.java
@@ -0,0 +1,17 @@
+package de.voelkldennis.backend.projects;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+@AutoConfigureMockMvc
+class ProjectControllerTest {
+
+  @DirtiesContext
+  @Test
+    void addNewProjectReturn() {
+    }
+}
