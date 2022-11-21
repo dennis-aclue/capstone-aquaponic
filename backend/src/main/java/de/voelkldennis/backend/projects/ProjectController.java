9e742d1c90162c53700275de598e7eb1faae0aca
dennis-aclue <voelkldennis@googlemail.com>
2022-11-21 11:50:10 +0100

    add post endpoint with Controler, Service, utils dto inside project package

---
+++ backend/src/main/java/de/voelkldennis/backend/projects/ProjectController.java
@@ -0,0 +1,23 @@
+package de.voelkldennis.backend.projects;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/projects")
+public class ProjectController {
+
+    private final ProjectService projectService;
+
+    public ProjectController(ProjectService projectService) {
+        this.projectService = projectService;
+    }
+
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    Project addNewProject(@RequestBody NewProjectDTO newProjectDTO ){
+        return projectService.addNewProject(newProjectDTO);
+    }
+
+
+}
---
+++ backend/src/main/java/de/voelkldennis/backend/projects/ProjectUtils.java
@@ -0,0 +1,11 @@
+package de.voelkldennis.backend.projects;
+
+import java.util.UUID;
+
+public class ProjectUtils {
+
+    public UUID generateUUID() {
+        return UUID.randomUUID();
+    }
+
+}
