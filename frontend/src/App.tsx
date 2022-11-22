08eb970a2db6eaace08373927266c77b6952eec5
dennis-aclue <voelkldennis@googlemail.com>
2022-11-22 14:53:05 +0100

    add some css style

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import {Route, Routes} from "react-router";
-import {BrowserRouter} from "react-router-dom";
 import About from "./pages/About";
 import HomePage from "./pages/Homes";
 import Projects from "./pages/Projects";
@@ -9,13 +8,11 @@ export default function App() {
     return <>
         <h1>Welcome everybody!</h1>
         <main>
-            <BrowserRouter>
                 <Routes>
                     <Route path="/" element={<HomePage />} />
                     <Route path="projects" element={<Projects />} />
                     <Route path="about" element={<About />} />
                 </Routes>
-            </BrowserRouter>
         </main>
         <footer></footer>
     </>;
