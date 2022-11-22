31d8f5bb80e40c618ee09ec3ba248291d85a3f71
dennis-aclue <voelkldennis@googlemail.com>
2022-11-22 17:27:56 +0100

    changed <div> to <nav> add new functionality after getting post request, set text, change button and make button not clickable during saving process

--- frontend/src/pages/Homes.tsx
+++ frontend/src/pages/Homes.tsx
@@ -4,10 +4,10 @@ import {Link} from "react-router-dom";
 export interface IHomePageProps {}
 const HomePage: React.FunctionComponent<IHomePageProps> = () => {
 
-    return <div>
+    return <nav>
         <p>This is the landing page.</p>
         <Link to="/about">Go to the about page</Link><br />
         <Link to="/projects">Go to the project page</Link><br />
-    </div>
+    </nav>
 };
 export default HomePage;
