4a289c97498cf6f9fb170bfbc68a04496ff84bf1
dennis-aclue <voelkldennis@googlemail.com>
2022-11-23 20:27:19 +0100

    change UUID to String because of problems with the mongoDb and get Method and added a Get endpoint for getting projects by id

--- backend/src/main/java/de/voelkldennis/backend/projects/ProjectService.java
+++ backend/src/main/java/de/voelkldennis/backend/projects/ProjectService.java
@@ -1,9 +1,8 @@
 package de.voelkldennis.backend.projects;
-
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import java.util.List;
-import java.util.UUID;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -13,7 +12,7 @@ public class ProjectService {
     private final ProjectRepo projectRepo;
 
     public Project addNewProject( NewProjectDTO newProjectDTO) {
-        UUID projectId = projectUtils.generateUUID();
+        String projectId = projectUtils.generateUUID();
         Project saveProject = new Project(projectId, newProjectDTO.projectName(), newProjectDTO.shortDescription(), newProjectDTO.projectVisibility(false));
         return projectRepo.save(saveProject);
     }
@@ -21,4 +20,9 @@ public class ProjectService {
     public List<Project> getAllProjects() {
         return projectRepo.findAll();
     }
+
+    public Optional<Project> getProjectWithId(String projectId) {
+        return projectRepo.findByProjectId(projectId);
+    }
+
 }
