ca91be7b4119555f36c7a12543faf0c65e284068
dennis-aclue <voelkldennis@googlemail.com>
2022-11-23 22:59:20 +0100

    save Frontend work, get method with Id and params does not work yet

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,20 +3,21 @@ import {Route, Routes} from "react-router";
 import HomePage from "./pages/Homes";
 import MemberOverview from "./pages/MemberOverview";
 import About from "./pages/About";
-import ProjectOverview from "./pages/ProjectOverview";
-import AddProject from "./pages/AddProject";
+import ProjectOverview from "./pages/projects/ProjectOverview";
+import AddProject from "./pages/projects/AddProject";
+import ProjectCard from "./pages/projects/ProjectCard";
 
 export default function App() {
     return <>
-        <h1>Welcome everybody!</h1>
         <main>
-                <Routes>
-                    <Route path="/" element={<HomePage />} />
-                    <Route path="/member" element={<MemberOverview />} />
-                    <Route path="/about" element={<About />} />
-                    <Route path="/projectOverview" element={<ProjectOverview />} />
-                    <Route path="/addProject" element={<AddProject />} />
-                </Routes>
+            <Routes>
+                <Route path="/" element={<HomePage/>}/>
+                <Route path="/member" element={<MemberOverview props={{username: "Dennis"}}/>}/>
+                <Route path="/about" element={<About/>}/>
+                <Route path="/projectOverview" element={<ProjectOverview/>}/>
+                <Route path="/addProject" element={<AddProject/>}/>
+                <Route path="/projectCard/:projectId" element={<ProjectCard />}/>
+            </Routes>
         </main>
         <footer>Aquaponic footer</footer>
     </>;
---
+++ frontend/src/pages/projects/ProjectModel.ts
@@ -0,0 +1,5 @@
+export type ProjectModel = {
+    projectId: string;
+    projectName: string;
+    shortDescription: string;
+}
