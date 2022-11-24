d07b03bf931b9a581f5c3cdd34bcf547e7e4d1a0
dennis-aclue <voelkldennis@googlemail.com>
2022-11-24 18:13:22 +0100

    Update getProjectWithIdAndReturnOk

--- backend/src/test/java/de/voelkldennis/backend/projects/ProjectControllerTest.java
+++ backend/src/test/java/de/voelkldennis/backend/projects/ProjectControllerTest.java
@@ -1,5 +1,6 @@
 package de.voelkldennis.backend.projects;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -18,6 +19,9 @@ class ProjectControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @DirtiesContext
     @Test
@@ -46,10 +50,30 @@ class ProjectControllerTest {
     @DirtiesContext
     @Test
     void getProjectWithIdAndReturnOk() throws Exception {
-        //given
-        String projectId = "add4ee6b-a702-4553-a0b0-2c07724b5b8b";
-        //when
-        mockMvc.perform(MockMvcRequestBuilders.get("/api/projects/projectCard/" + projectId))
-                .andExpect(status().is(200));
+
+        String content = mockMvc.perform(MockMvcRequestBuilders.post("/api/projects")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("""
+                                {
+                                    "projectName": "aquaponic_test_name",
+                                    "shortDescription": "aquaponic_test_description"
+                                }
+                                """))
+                .andExpect(status().is(201))
+                .andReturn().getResponse().getContentAsString();
+
+        Project project = objectMapper.readValue(content, Project.class);
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/projects/projectCard/" + project.projectId()))
+                .andExpect(status().is(200))
+                .andExpect(content().json("""
+                        {
+                            "projectId": "<ID>",
+                            "projectName": "aquaponic_test_name",
+                            "shortDescription": "aquaponic_test_description",
+                            "projectVisibility": false
+                        }
+                        """.replace("<ID>", project.projectId())));
     }
+
 }
