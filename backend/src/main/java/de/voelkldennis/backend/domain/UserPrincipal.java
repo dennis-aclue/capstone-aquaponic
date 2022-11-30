607329dcb6c51aa7fe75ddcbda84ac70ff2fe574
dennis-aclue <voelkldennis@googlemail.com>
2022-11-30 12:03:06 +0100

    add getter and setter and constructor with all parameters and default add javax.persistence spring-boot-starter-data-jpa, spring-security-core, spring-security-test to pom.xml add httpResponse add UserPrincipals

---
+++ backend/src/main/java/de/voelkldennis/backend/domain/UserPrincipal.java
@@ -0,0 +1,56 @@
+package de.voelkldennis.backend.domain;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+import static java.util.Arrays.stream;
+
+public class UserPrincipal implements UserDetails {
+    private User user;
+
+    public UserPrincipal(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        // return any collections of authorities (roles) that this user has
+        return stream(this.user.getAuthorities()).map(SimpleGrantedAuthority::new).collect(Collectors.toList());
+    }
+
+    @Override
+    public String getPassword() {
+        return this.user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return this.user.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        // I do not use this method in my application now
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return this.user.isNotLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        // I do not use this method too (I do not use password expiration)
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return this.user.isActive();
+    }
+}
