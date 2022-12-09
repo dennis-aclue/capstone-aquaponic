9f62428fb8bd519c4522babb3ca307533611efd7
dennis-aclue <voelkldennis@googlemail.com>
2022-12-09 14:36:57 +0100

    update projectController Tests

--- backend/src/main/java/de/voelkldennis/backend/projects/ProjectController.java
+++ backend/src/main/java/de/voelkldennis/backend/projects/ProjectController.java
@@ -4,7 +4,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.util.Base64;
 import java.util.List;
 import java.util.Optional;
 
@@ -13,8 +12,6 @@ import java.util.Optional;
 public class ProjectController {
 
     private final ProjectService projectService;
-
-    Base64.Decoder decoder = Base64.getUrlDecoder();
 
     public ProjectController(ProjectService projectService) {
         this.projectService = projectService;
@@ -30,27 +27,6 @@ public class ProjectController {
     List<Project> getAllProjects() {
         return projectService.getAllProjects();
     }
-
-    /*@GetMapping("/userProjectOverview/{")
-    List<Project> getUserProjects(@RequestHeader HttpHeaders headers){
-        System.out.println(headers);
-
-        System.out.println(SecurityContextHolder.getContext().getAuthentication());
-        System.out.println(SecurityContextHolder.getContext().getAuthentication().getCredentials().toString());
-        System.out.println(SecurityContextHolder.getContext().getAuthentication().getDetails().toString());
-        System.out.println(SecurityContextHolder.getContext().getAuthentication().getPrincipal().toString());
-
-
-        //String header = new String(decoder.decode(chunks[0]));
-        //String payload = new String(decoder.decode(chunks[1]));
-
-        //System.out.println(header);
-        //System.out.println(payload);
-        //String username = payload.split(":")[1].split(",")[0].replace("\"", "");
-        //System.out.println(username);
-
-        return null;
-    }*/
 
     @GetMapping("/userProjectOverview/{userId}")
     List<Project> getUserProjects(@PathVariable String userId) {
